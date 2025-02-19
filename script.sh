2095d6bb31614883bfd32a475befddd57834345a
Pablo-Cas <pablocastrillo2005@gmail.com>
2025-02-19 23:49:15 +0100

    Instalar y configuración básica por comandos
    Lo basico para que postfix este instalado y configurado con un dominio

--- script.sh
+++ script.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-Aqui va el script
+
+echo "Iniciando instalación de Postfix..."
+sudo apt update && sudo apt install -y postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+
+echo "¿Cómo quieres que se llame tu dominio? (ejemplo.com)"
+read -p "Dominio: " dominio
+
+sudo postconf -e "home_mailbox= Maildir/"
+sudo postconf -e "inet_interfaces = all"
+sudo postconf -e "mydestination = localhost, $dominio"
+
+sudo systemctl restart postfix
+
+echo "Postfix instalado y configurado"
