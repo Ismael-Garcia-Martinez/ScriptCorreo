761f8136f3c2607e99879573d79a1af75334c31d
Pablo-Cas <pablocastrillo2005@gmail.com>
2025-02-20 00:23:47 +0100

    Menu y desinstalacion
    Aqui hago un menu sencillo con condicionales y le meto la opcion de desintalar con comandos.

--- script.sh
+++ script.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-echo "Iniciando instalación de Postfix..."
-sudo apt update && sudo apt install -y postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+echo "1) Instalar Postfix"
+echo "2) Desinstalar Postfix"
+read -p "Seleccione una opción: " opcion
 
-echo "¿Cómo quieres que se llame tu dominio? (ejemplo.com)"
-read -p "Dominio: " dominio
+if [ "$opcion" -eq 1 ]; then
+	sudo apt update && sudo apt install -y postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
 
-sudo postconf -e "home_mailbox= Maildir/"
-sudo postconf -e "inet_interfaces = all"
-sudo postconf -e "mydestination = localhost, $dominio"
+	echo "¿Cómo quieres que se llame tu dominio? (ejemplo.com)"
+	read -p "Dominio: " dominio
 
-sudo systemctl restart postfix
+	sudo postconf -e "home_mailbox= Maildir/"
+	sudo postconf -e "inet_interfaces = all"
+	sudo postconf -e "mydestination = localhost, $dominio"
 
-echo "Postfix instalado y configurado"
+	sudo systemctl restart postfix
+
+	echo "Postfix instalado y configurado"
+
+elif [ "$opcion" -eq 2 ]; then
+    sudo systemctl stop postfix
+    sudo apt remove --purge -y postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+    sudo apt autoremove -y
+    echo "Postfix desinstalado"
+fi
