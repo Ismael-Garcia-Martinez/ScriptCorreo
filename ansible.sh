d646461d41358aac69d861efd81b3f7dccad3889
IsmaelLM30 <ismael8martin@gmail.com>
2025-02-25 10:09:52 +0100

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -12,4 +12,46 @@ else
     echo "Error: No se pudo crear el directorio '$Director>
     exit 1
 fi
+cat > "$archivo" <<EOL
+---
+- name: Instalar y configurar Postfix
+  hosts: servidores_correo
+  become: true
+  vars:
+    postfix_relayhost: "[relay.example.com]:587"
+  
+  tasks:
+    - name: Instalar Postfix
+      apt:
+        name: postfix
+        state: present
+        update_cache: yes
+
+    - name: Configurar Postfix en modo proxy
+      template:
+        src: configuracion/principal.cf.j2
+        dest: /etc/postfix/main.cf
+      notify: Reiniciar Postfix
+
+    - name: Asegurar que Postfix está habilitado y activo
+      systemd:
+        name: postfix
+        enabled: yes
+        state: started
+   
+  handlers:
+    - name: Reiniciar Postfix
+      service:
+        name: postfix
+        state: restarted
+EOL
+
+if [ -f "$archivo" ]; then
+    echo "Playbook creado correctamente en '$archivo'."
+else
+    echo "Error: No se pudo crear el playbook."
+    exit 1
+fi
+ 
+
 
